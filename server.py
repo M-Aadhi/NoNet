06137d06c41ddc144579ae3d097d7718af8df795
M-Aadhi <ezioabaddon@gmail.com>
2024-08-31 14:03:36 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_socketio import SocketIO, send
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+socketio = SocketIO(app)
+
+# Ensure the upload folder exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        # Notify all clients about the new file
+        socketio.emit('new_file', {'filename': file.filename})
+        return redirect(url_for('index'))
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
